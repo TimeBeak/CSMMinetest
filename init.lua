5321b8f86997d9655502414dda7c5735625f3e78
Niden <80420845+Niden1@users.noreply.github.com>
2021-09-05 18:55:28 -0300

    Update
    Update

--- init.lua
+++ init.lua
@@ -1,16 +1,18 @@
---Death Coords
+--Death Coords By Niden
 
+--Chat Message
 core.register_on_death(function()
-  local pos = minetest.localplayer:get_pos(); minetest.display_chat_message((core.get_color_escape_sequence("#FF00FF") .."*** CSM - DEAD AT: " .. pos.x .. ", " .. pos.y .. ", " .. pos.z ))
+  local pos = minetest.localplayer:get_pos(); minetest.display_chat_message((core.get_color_escape_sequence("#FF0000") .."*** CSM - LAST DEAD AT: " .. pos.x .. ", " .. pos.y .. ", " .. pos.z ))
 end
 )
---No Support 4.17.0
+
+--FormSpec
 core.register_on_death(function()
 	core.display_chat_message("You died.")
 	local pos = minetest.localplayer:get_pos();
-	local formspec = "size[11,5.5]bgcolor[#320000b4;true]" ..
-		"label[2,1.40;" .. fgettext("                                 You Die      \n \nDEAD AT: " .. pos.x .. " " .. pos.y .. " " .. pos.z) ..
-		"]button_exit[4,3;3,0.5;btn_respawn;".. fgettext("Respawn") .."]"
+	local formspec = "size[16,9]bgcolor[#320000b4;true]" ..
+		"label[4,1.60;" .. fgettext("                                       YOU DIE.      \n \nDEAD AT: " .. pos.x .. " " .. pos.y .. " " .. pos.z) ..
+		"]button_exit[6.6,8;3,0.8;btn_respawn;".. fgettext("OK") .."]"
 	core.show_formspec("bultin:death", formspec)
 end)
 
